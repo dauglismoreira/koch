b6d6064be5eba9d0d13d3911e10c63f3f1518786
Dauglis Moreira <dauglisc@gmail.com>
2023-10-20 16:34:54 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import StyledComponentsRegistry from './lib/registry'
 import { OpenSans } from './fonts'
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -29,6 +30,9 @@ export default function RootLayout({
   
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className={`${inter.className} ${OpenSans.className}`}>
           <StyledComponentsRegistry>
             <Header/>
