6a92678fa8c6005c5d53d0755e3fd6cbfebf8bec
Dauglis Moreira <dauglisc@gmail.com>
2023-12-04 16:17:41 -0300

    fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {}
 
 module.exports = {
-    reactStrictMode: true,
+    // reactStrictMode: true,
     images: {
       domains: ['api.kochconstrutora.com.br', 'koch.impacte.com.br', 'localhost:8000'],
     },
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import StyledComponentsRegistry from './lib/registry'
 import { OpenSans } from './fonts'
 import Head from 'next/head';
 import { GoogleTagManager } from '@next/third-parties/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -41,9 +42,15 @@ export default function RootLayout({
             <Header/>
             {children}
             <Footer/>
+            <Script
+              src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/cdcc6f84-8caa-405e-8067-6bf007837cc3-loader.js"
+              nonce="XUENAJFW"
+              data-test="script"
+            />
           </StyledComponentsRegistry>
 
-          {/* <script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/cdcc6f84-8caa-405e-8067-6bf007837cc3-loader.js"></script> */}
+
+          {/* <script type="text/javascript" async src=""></script> */}
         </body>
     </html>
   )
